19312a4b5896f9f96f27114de921af58e061b0f4
杨培 <15172338506@163.com>
2018-11-19 11:00:29 +0800

    Initial Commit

---
+++ YPCellSwipe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YPCellSwipe
+//
+//  Created by hbck on 2018/11/19.
+//  Copyright © 2018年 com.hbck.mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YPCellSwipe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YPCellSwipe
+//
+//  Created by hbck on 2018/11/19.
+//  Copyright © 2018年 com.hbck.mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
